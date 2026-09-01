56c02c5d45034d8e982e626f2538a83bfe15c25c
Letícia Gonçalves <leti.morais48@gmail.com>
2026-09-01 16:03:08 -0300

    added second implementation lecture exercises

---
+++ UnidadeI/exemplos_site/lecture2/wireframe.c
@@ -0,0 +1,40 @@
+/**
+ * \file wireframe.c
+ *
+ * \brief Implementação do arquivo principal de renderização do modelo 3D.
+ *
+ * \author
+ * Petrucio Ricardo Tavares de Medeiros \n
+ * Universidade Federal Rural do Semi-Árido \n
+ * Departamento de Engenharias e Tecnologia \n
+ * petrucio at ufersa (dot) edu (dot) br
+ *
+ * \version 1.0
+ * \date May 2025
+ */
+
+#include <stdio.h>
+#include "model.h"
+Vertex vertices[MAX_VERTICES];
+Face faces[MAX_FACES];
+
+int vcount, fcount;
+int main() {
+
+    // printf("teste");
+
+    clr();
+
+    // Lê o arquivo OBJ enviado
+    if (!load_obj("models/robot.obj", vertices, &vcount, faces, &fcount)) {
+        printf("falha");
+        return 1;
+    }
+
+    // Renderiza as faces no framebuffer
+    render_faces(vertices, faces, vcount, fcount);
+
+    save();
+    
+    return 0;
+}
